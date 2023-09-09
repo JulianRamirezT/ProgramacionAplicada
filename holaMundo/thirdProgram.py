3b03b5a4070b2df192f834fb501a86741bb72c35
JulianRamirezT <142341282+JulianRamirezT@users.noreply.github.com>
2023-09-08 21:17:36 -0500

    Create thirdProgram.py

---
+++ holaMundo/thirdProgram.py
@@ -0,0 +1,15 @@
+# Estamos defininedo una variable 
+meal = "An english muffins"
+
+#imprimimos 
+print("Breakast: ")
+print(meal)
+
+meal = "A colombian recipe"
+
+#imprimimos 
+print("Lunch: ")
+print(meal)
+
+print("Dinner: ")
+print(meal)
