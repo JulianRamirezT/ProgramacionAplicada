ebd4027272f04dc06a1d406ca3f624f1ca0c9c46
JulianRamirezT <142341282+JulianRamirezT@users.noreply.github.com>
2023-11-23 17:53:32 -0500

    Create threadsSincr.py

---
+++ segundoCorte/threads/threadsSincr.py
@@ -0,0 +1,21 @@
+#sincronizacion 
+import threading
+from time import sleep
+import random
+
+def ejecutar():
+    print(f'comienza {threading.current_thread().name}')
+    sleep(random.random()) #esperamos un tiempor aleatorio entre 0 y 1
+    print(f'Termina {threading.current_thread().name}')
+
+#creamos los hilos 
+
+hilo1 = threading.Thread(target=ejecutar, name='Hilo 1')
+hilo2 = threading.Thread(target=ejecutar, name='Hilo 2')
+
+#ejecutamos los hilos 
+
+hilo1.start()
+hilo2.start()
+
+print('El hilo principal no espera por el resto de los hilos')
